28970afe964da2f9f1153a69bb1fa9224b4fe565
SoImKiyo <85789841+SoimKiyo@users.noreply.github.com>
2025-02-09 02:10:08 +0100

    cinematic test code

--- sfx.py
+++ sfx.py
@@ -39,6 +39,14 @@ menuconfirm_sound = pygame.mixer.Sound("assets/audio/sfx/ui/menu_confirm.wav")
 menuup_sound = pygame.mixer.Sound("assets/audio/sfx/ui/menu_up.wav")
 menudown_sound = pygame.mixer.Sound("assets/audio/sfx/ui/menu_down.wav")
 powerup_sound = pygame.mixer.Sound("assets/audio/sfx/ui/menu_powerup.wav")
+dialog_sound = pygame.mixer.Sound("assets/audio/sfx/ui/dialog_text.wav")
+
+# Cinematic
+scene1_sound = pygame.mixer.Sound("assets/audio/sfx/cinematic/scene1.mp3")
+scene2_sound = pygame.mixer.Sound("assets/audio/sfx/cinematic/scene2.mp3")
+scene3_sound = pygame.mixer.Sound("assets/audio/sfx/cinematic/scene3.mp3")
+scene4_sound = pygame.mixer.Sound("assets/audio/sfx/cinematic/scene4.mp3")
+scene5_sound = pygame.mixer.Sound("assets/audio/sfx/cinematic/scene5.mp3")
 
 def set_volume_all(volume):
     """
@@ -79,3 +87,11 @@ def set_volume_all(volume):
     menuup_sound.set_volume(volume)
     menudown_sound.set_volume(volume)
     powerup_sound.set_volume(volume)
+    dialog_sound.set_volume(volume)
+
+    # Cinematic
+    scene1_sound.set_volume(volume)
+    scene2_sound.set_volume(volume)
+    scene3_sound.set_volume(volume)
+    scene4_sound.set_volume(volume)
+    scene5_sound.set_volume(volume)
