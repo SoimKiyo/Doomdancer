4e0a49275901e8d93e1fe857918695e04ecea52d
Kiyoni <85789841+SoimKiyo@users.noreply.github.com>
2025-02-12 09:03:09 +0100

    commentaire dans player

--- player.py
+++ player.py
@@ -7,93 +7,97 @@ from sfx import collect_sound, damage_sound, damagevoice_sound1, damagevoice_sou
 
 joysticks = []  # Liste vide pour stocker les manettes
 
+# Fonction pour redimenssioner les images
 def scale_img(image, scale):
     w = image.get_width()
     h = image.get_height()
     return pygame.transform.scale(image, (int(w * scale), int(h * scale)))
 
+# Fonction pour l'animation du joueur
 def player_animations():
     # Dictionnaire contenant les types d'animations et leur nombre de frames 
-    animation_frames = { 
+    animation_frames = {
         "idle": 30, 
         "run": 8,
         "slash": 11
     }
     animation_list = {key: [] for key in animation_frames}  # Dictionnaire pour stocker les animations 
-    for animation, num_frames in animation_frames.items():
-        for i in range(num_frames):
+    for animation, num_frames in animation_frames.items(): # Pour chacune des animations, et chacune de leur frames
+        for i in range(num_frames): # Pour chaque frames
             # Charger chaque sprite et le redimensionner
             img = pygame.image.load(f"assets/images/player/{animation}/{i}.png").convert_alpha()
             img = scale_img(img, SCALE)
-            animation_list[animation].append(img)
+            animation_list[animation].append(img) # On ajoute chaque image dans la liste de l'animation
     return animation_list
 
+# Classe du Joueur
 class Player:
     def __init__(self, x, y, width, height, animation_list):
         # Animation / Sprite du personnage
         self.flip = False  # Permet de retourner l'image du joueur
         self.animation_list = animation_list
         self.frame_index = 0  # Frame de départ
-        self.action = "idle"
+        self.action = "idle" # Animation de base
         self.update_time = pygame.time.get_ticks()  # Temps de mise à jour de l'animation
-        self.running = False
-        self.is_attacking = False
-        self.image = self.animation_list[self.action][self.frame_index]
+        self.running = False # Le joueur est-il entrain de courir ?
+        self.is_attacking = False # Le joueur est-il entrain d'attaquer ?
+        self.image = self.animation_list[self.action][self.frame_index] # On récupère l'image de l'animation et de la frame donné
         # Rectangle du joueur
         self.rect = pygame.Rect(x, y, width, height)
-        self.rect.center = (x, y)
+        self.rect.center = (x, y) # Centrer la box
         self.speed = PLAYER_SPEED  # Vitesse de déplacement
         self.screen_rect = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)  # Limites de l'écran
         # Création d'une hitbox
-        self.hitbox = pygame.Rect(0, 0, width - 10, height - 10)
-        self.hitbox.center = self.rect.center
+        self.hitbox = pygame.Rect(0, 0, width - 10, height - 10) # Nouvelle box représentant la hitbox
+        self.hitbox.center = self.rect.center # On centre la hitbox
 
-        self.hide_weapon = False
+        self.hide_weapon = False # L'arme doit-elle être caché
         
         # Vie du joueur
-        self.max_health = 100  # Vie maximale
-        self.health = self.max_health  # Vie actuelle
-        self.alive = True
-        self.is_invincible = False  # Empêche les dégâts en boucle
-        self.invincibility_timer = 0  # Temps d'invincibilité après un coup
-        self.deaths = 0  # Nombre de fois où le joueur est mort
+        self.max_health = 100 # Vie maximale
+        self.health = self.max_health # Vie actuelle
+        self.alive = True # Le joueur est-il vivant ?
+        self.is_invincible = False # Empêche les dégâts en boucle
+        self.invincibility_timer = 0 # Temps d'invincibilité après un coup
+        self.deaths = 0 # Nombre de fois où le joueur est mort
 
         self.coins = 0 # Nombre de fragments du joueur
 
         # Pour mémoriser la dernière direction de déplacement (normalisée)
-        self.last_dx = 0
-        self.last_dy = 0
+        self.last_dx = 0 # dernière valeur Delta X
+        self.last_dy = 0 # dernière valeur Delta Y
         # Pour conserver la dernière position connue de la souris
-        self.last_mouse_pos = None
+        self.last_mouse_pos = None # Dernière position de la souris enregistré
         self.using_mouse = False # La souris est-elle utilisé ?
 
         self.using_gamepad = False  # Indique si le joueur utilise une manette
 
-        self.last_step_sound_time = 0
-        self.distance_since_last_step = 0
+        self.last_step_sound_time = 0 # Dernière utilisation du son de la souris
 
-        self.dash_active = False
+        self.dash_active = False # Le dash est il actif (en cours d'utilisation) ?
         self.dash_duration = 300 # Durée du dash en millisecondes
         self.dash_cooldown = 1300 # Cooldown entre dash
-        self.last_dash_time = 0
+        self.last_dash_time = 0 # Temps depuis le dernié dash
         self.dash_multiplier = 3 # Facteur de multiplication de la vitesse durant le dash
         self.dash_trigger_released = True # Pour éviter les déclenchements répétés
 
-
+    # Fonction pour mettre à jour les limites de l'écran
     def update_screen_limits(self, screen_width, screen_height):
         self.screen_rect = pygame.Rect(0, 0, screen_width, screen_height)
 
+    # Fonction pour ramasser les pièces
     def collect_coin(self):
         self.coins += 1
         collect_sound.play()
 
+    # Fonction pour les mouvements du joueurs
     def move(self, keys, screen_rect, weapon, obstacle_tiles, exit_tile, can_exit):
         if self.alive == False:  # Si le joueur est mort, il ne peut pas bouger
-            return [0, 0], False
+            return [0, 0], False # On retourne [0,0] pour le screen_scroll, et False pour le level_complete
 
-        screen_scroll = [0, 0]
-        level_complete = False
-        self.running = False
+        screen_scroll = [0, 0] # On initialise le défilement de l'écran
+        level_complete = False # On initialise la boolean du niveau_complété
+        self.running = False # On initialise la boolean pour savoir si on cours ou non
 
         # Entrées clavier (ZQSD/Flèches directionnel)
         dx_keyboard = (keys[pygame.K_d] or keys[pygame.K_RIGHT]) - (keys[pygame.K_q] or keys[pygame.K_LEFT])
@@ -102,25 +106,24 @@ class Player:
         # Entrées manettes (Dpad/Joystick Gauche)
         dx_gamepad = 0
         dy_gamepad = 0
-        for joystick in joysticks:
+        for joystick in joysticks: # Pour chacune des manettes dans la liste des manettes
             # Joystick gauche
             if joystick.get_numaxes() >= 2:
-                horiz_move = joystick.get_axis(0)
-                vert_move = joystick.get_axis(1)
-                if abs(horiz_move) > 0.15:
+                horiz_move = joystick.get_axis(0) # Déplacement horizontal
+                vert_move = joystick.get_axis(1) # Déplacement vertical
+                if abs(horiz_move) > 0.15: # Marge pour vérifier qu'il y a un minimum de mouvement sur le joystick
                     dx_gamepad += horiz_move
-                if abs(vert_move) > 0.15:
+                if abs(vert_move) > 0.15: # Marge pour vérifier qu'il y a un minimum de mouvement sur le joystick
                     dy_gamepad += vert_move
             # D-Pad
             if joystick.get_numhats() > 0:
-                hat_x, hat_y = joystick.get_hat(0)
-                if hat_x != 0:
+                hat_x, hat_y = joystick.get_hat(0) # Mouvement vetical/horizontal
+                if hat_x != 0: # Si on appuie sur une flèche du dpad
                     dx_gamepad += hat_x
-                if hat_y != 0:
+                if hat_y != 0: # Si on appuie sur une flèche du dpad
                     dy_gamepad -= hat_y
         # Vérifie si le joueur utilise une manette
         self.using_gamepad = any(abs(joystick.get_axis(0)) > 0.15 or abs(joystick.get_axis(1)) > 0.15 for joystick in joysticks)
-
 
         # Calcul du déplacement total
         dx = dx_keyboard + dx_gamepad
@@ -128,7 +131,7 @@ class Player:
 
         # Mise à jour de l'info sur la souris
         rel = pygame.mouse.get_rel()  # Renvoie le mouvement relatif depuis le dernier appel
-        if rel != (0, 0):
+        if rel != (0, 0): # S'il y a un mouvement
             self.using_mouse = True
             self.last_mouse_pos = pygame.mouse.get_pos()
         # Si aucun mouvement n'est détecté et qu'on n'a jamais eu d'info sur la souris, on garde les mouvements du clavier
@@ -137,21 +140,21 @@ class Player:
 
         # Gestion du dash
         current_time = pygame.time.get_ticks()
-        dash_input = keys[pygame.K_SPACE]
+        dash_input = keys[pygame.K_SPACE] # Barre espace sur le clavier pour déclencher le dash
         for joystick in joysticks:
-            if joystick.get_button(0):
+            if joystick.get_button(0): # Bouton A pour dash sur manette
                 dash_input = True
-                break
+                break # On continue le déroulé de la fonction
 
-        if dash_input:
-            if self.dash_trigger_released and (current_time - self.last_dash_time >= self.dash_cooldown) and not self.dash_active:
+        if dash_input: # Si une touche de dash est déclenché
+            if self.dash_trigger_released and (current_time - self.last_dash_time >= self.dash_cooldown) and not self.dash_active: # Si le cooldown est fini et qu'on n'est pas entrain de dash
                 dash_sound.play()
-                self.dash_active = True
-                self.dash_trigger_released = False
-                self.last_dash_time = current_time
-                self.dash_start_time = current_time
+                self.dash_active = True # On marque le dash comme actif
+                self.dash_trigger_released = False # On marque la touche du dash comme appuyé pour éviter les répétitions
+                self.last_dash_time = current_time # On met à jour la date de la dernière utilisation du dash
+                self.dash_start_time = current_time # On met à jour la date du début du dash
         else:
-            self.dash_trigger_released = True
+            self.dash_trigger_released = True # On marque la touche du dash comme relaché
         
         # Si le dash est actif, on force l'invincibilité
         if self.dash_active:
@@ -159,10 +162,10 @@ class Player:
 
         # Si le joueur se déplace
         if dx != 0 or dy != 0:
-            self.running = True
+            self.running = True # Le joueur cours donc
             # Priorité à la souris pour le flip si elle a bougé
-            if self.using_mouse and self.last_mouse_pos:
-                self.flip = self.last_mouse_pos[0] < self.rect.centerx
+            if self.using_mouse and self.last_mouse_pos: # Si la souris est utilisé et qu'il y a une valeur renvoyé par last_mouse_pos qui n'est pas null
+                self.flip = self.last_mouse_pos[0] < self.rect.centerx # On tourne le sprite du personnage par rapport à la position de la souris
             # Sinon, on se base sur la direction clavier (ou manette)
             else:
                 # Pour le clavier (dx_keyboard)
@@ -174,41 +177,42 @@ class Player:
 
             # Normalisation de la direction pour conserver le dernier vecteur de déplacement
             norm = math.sqrt(dx ** 2 + dy ** 2)
-            if norm != 0:
-                norm_dx = dx / norm
-                norm_dy = dy / norm
-            else:
+            if norm != 0: # Si la norme n'est pas 0
+                norm_dx = dx / norm # On divise la valeur delta X par la norme
+                norm_dy = dy / norm # On divise la valeur delta Y par la norme
+            else: # Sinon on renvoie 0 pour les deux valeurs (x,y)
                 norm_dx, norm_dy = 0, 0
 
+            # On met à jour le dernier déplacement du joueur
             self.last_dx = norm_dx
             self.last_dy = norm_dy
 
             # Appliquer le dash si actif
-            if self.dash_active:
+            if self.dash_active: # Si le dash est actif on multiplie le déplacement par le boost du dash
                 dx = norm_dx * self.speed * self.dash_multiplier
                 dy = norm_dy * self.speed * self.dash_multiplier
-            else:
+            else: # Sinon on renvoie la vitesse normal du joueur
                 dx = norm_dx * self.speed
                 dy = norm_dy * self.speed
 
         # Appliquer le déplacement et regarder les collisions
-        self.rect.x += dx
-        for obstacle in obstacle_tiles:
+        self.rect.x += dx # Pour les mouvements horizontal
+        for obstacle in obstacle_tiles: # S'il y a des obstacles dans la liste des obstacles
             # Regarder les collisions
             if obstacle[1].colliderect(self.rect):
                 # Regarder de qu'elle côté et la collision
-                if dx > 0:
+                if dx > 0: # Si elle est a droite
                     self.rect.right = obstacle[1].left
-                if dx < 0:
+                if dx < 0: # Si elle est a gauche
                     self.rect.left = obstacle[1].right
-        self.rect.y += dy
-        for obstacle in obstacle_tiles:
+        self.rect.y += dy # Pour les mouvements vertical
+        for obstacle in obstacle_tiles: # S'il y a des obstacles dans la liste des obstacles
             # Regarder les collisions
             if obstacle[1].colliderect(self.rect):
                 # Regarder de qu'elle côté et la collision
-                if dy > 0:
+                if dy > 0: # Si elle est en bas
                     self.rect.bottom = obstacle[1].top
-                if dy < 0:
+                if dy < 0: # Si elle est en haut
                     self.rect.top = obstacle[1].bottom
 
         # Mise à jour de la hitbox pour qu'elle suive le joueur
@@ -216,11 +220,11 @@ class Player:
 
         # Vérifier la collission avec l'élément permettant de changer de niveau
         for tile in exit_tile:
-            if tile[1].colliderect(self.rect):
-                if can_exit:
-                    level_complete = True
+            if tile[1].colliderect(self.rect): # S'il y a une collision
+                if can_exit: # Si le joueur peut sortir du niveau (c'est à dire s'il n'y a plus d'ennemie vivant dans celui-ci)
+                    level_complete = True # On marque le niveau comme fini
                     break
-                else :
+                else : # Sinon
                     overlap_rect = self.rect.clip(tile[1])
                     if overlap_rect.width < overlap_rect.height:
                         if self.rect.centerx < tile[1].centerx:
@@ -267,23 +271,25 @@ class Player:
 
         return screen_scroll, level_complete
     
+    # Fonction pour mettre des dégâts au joueur
     def take_damage(self, damage):
         # Si le dash est actif, on ignore les dégâts
         if self.dash_active:
             return
-        if not self.is_invincible:
-            self.health -= damage
+        if not self.is_invincible: # Si le joueur n'est pas invincible
+            self.health -= damage # On lui enlève le nombre de dégât à sa vie
             damage_sound.play()
             voice_sounds = [damagevoice_sound1, damagevoice_sound2, damagevoice_sound3]
             choice(voice_sounds).play()
-            if self.health <= 0:
+            if self.health <= 0: # Si le joueur est mort
                 self.health = 0  # Empêche la vie d'aller en dessous de 0
                 death_sound.play()
                 self.alive = False
-                self.deaths += 1
+                self.deaths += 1 # On rajoute 1 au compteur de mort
             #self.is_invincible = True
             self.invincibility_timer = pygame.time.get_ticks()
 
+    # Fonction pour mettre à jour le joueur
     def update(self):
         current_time = pygame.time.get_ticks()
         animation_cooldown = 60  # Valeur par défaut
@@ -298,22 +304,24 @@ class Player:
             self.update_action("idle")
             animation_cooldown = 60
 
-        self.image = self.animation_list[self.action][self.frame_index]
-        if current_time - self.update_time > animation_cooldown:
-            self.frame_index += 1
-            self.update_time = current_time
-        if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index] # On récupère l'image du sprite par rapport à l'action/la frame
+        if current_time - self.update_time > animation_cooldown: # Si le cooldown est passer
+            self.frame_index += 1 # On passe à la frame suivante
+            self.update_time = current_time # On met à jour le temps pour le remettre à zéro
+        if self.frame_index >= len(self.animation_list[self.action]): # Si la frame actuel dépasse la dernière frame
+            self.frame_index = 0 # On repasse à la première
 
         if self.is_invincible and current_time - self.invincibility_timer > 1000:
             self.is_invincible = False  # Désactive l'invincibilité après 1 seconde
 
+    # Fonction pour mettre à jour les actions du joueurs
     def update_action(self, new_action):
         if new_action != self.action:
             self.action = new_action
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
 
+    # Fonction pour dessiner le joueur
     def draw(self, surface):
         if not self.dash_active:
             flipped_image = pygame.transform.flip(self.image, self.flip, False)
@@ -323,12 +331,14 @@ class Player:
         #pygame.draw.rect(surface, PLAYER_COLOR, self.hitbox, 1)
 
 
+# Classe des PowerUP 
 class PowerUP:
     def __init__(self, player, activepowerups):
         self.player = player
-        self.activepowerups = activepowerups
+        self.activepowerups = activepowerups # Liste des pouvoirs actifs
         self.apply_powerups()  # Appliquer immédiatement les effets
 
+    # Fonction pour appliquer les effets des améliorations
     def apply_powerups(self):
         # Powerup "speed" : double la vitesse s'il est actif
         if "speed" in self.activepowerups:
